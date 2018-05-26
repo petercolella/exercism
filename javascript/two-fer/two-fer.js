8d51f56b27ef79ce19b87606172ab1c7f3dd4e68
petercolella <peter.joseph.colella@gmail.com>
2018-05-26 00:28:47 -0400

    two-fer

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,17 @@
+var TwoFer = function () {};
+
+TwoFer.prototype.twoFer = function (who) {
+  // your code goes here
+  // You will have to use the parameter who
+  // in some way. In this example, it is just
+  // returned, but your solution will have to
+  // use a conditional.
+  if (who) {
+    return 'One for ' + who + ', one for me.';
+  } else {
+    return 'One for you, one for me.';
+  }
+  // return who;
+};
+
+module.exports = TwoFer;
