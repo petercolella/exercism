a8bd6619b3524615d059ddb94e4b809e3e377c1f
petercolella <peter.joseph.colella@gmail.com>
2018-05-30 02:13:55 -0400

    passed final two tests

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -2,19 +2,38 @@ var DnaTranscriber = function (input) {}
 
 DnaTranscriber.prototype.toRna = function (dna) {
 
-    const replaceChars = {
-        'G' : 'C',
-        'C' : 'G',
-        'T' : 'A',
-        'A' : 'U'
-    };
-    
-    const rna = dna
-        .replace(/G|C|T|A/g, function(match) {
-            return replaceChars[match];
-        });
+    const removeChars = {
+        'G' : '',
+        'C' : '',
+        'T' : '',
+        'A' : ''
+    }
 
-    return rna;
+    const remainingChars = dna
+        .replace(/G|C|T|A/g, function(match) {
+            return removeChars[match];
+        })
+
+    if (remainingChars) {
+
+        throw new Error('Invalid input');
+
+    } else {
+    
+        const replaceChars = {
+            'G' : 'C',
+            'C' : 'G',
+            'T' : 'A',
+            'A' : 'U'
+        }
+        
+        const rna = dna
+            .replace(/G|C|T|A/g, function(match) {
+                return replaceChars[match];
+            })
+
+        return rna;
+    }
 }
 
 module.exports = DnaTranscriber;
