59dcde027edec7543ae794378eaaf674ec423880
petercolella <peter.joseph.colella@gmail.com>
2018-06-02 23:39:46 -0400

    refactored solution

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,39 +1,22 @@
 var DnaTranscriber = function (input) {}
 
 DnaTranscriber.prototype.toRna = function (dna) {
-
-    const removeChars = {
-        'G' : '',
-        'C' : '',
-        'T' : '',
-        'A' : ''
-    }
-
-    const remainingChars = dna
-        .replace(/G|C|T|A/g, function(match) {
-            return removeChars[match];
-        })
-
-    if (remainingChars) {
-
-        throw new Error('Invalid input');
-
-    } else {
     
-        const replaceChars = {
-            'G' : 'C',
-            'C' : 'G',
-            'T' : 'A',
-            'A' : 'U'
-        }
-        
-        const rna = dna
-            .replace(/G|C|T|A/g, function(match) {
-                return replaceChars[match];
-            })
-
-        return rna;
-    }
+    const rnaCompliments = {
+        'G' : 'C',
+        'C' : 'G',
+        'T' : 'A',
+        'A' : 'U'
+    };
+    
+    return dna
+        .split('')
+        .map(function(nucleotide) {
+            if (!rnaCompliments[nucleotide])
+                throw new Error('Invalid input');
+            return rnaCompliments[nucleotide];
+        })
+        .join('');
 }
 
 module.exports = DnaTranscriber;
