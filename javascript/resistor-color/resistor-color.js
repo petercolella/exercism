4cc091193be255b062bcfc82549ef7d40d606b6d
petercolella <peter.joseph.colella@gmail.com>
2019-04-20 23:03:44 -0400

    resistor color passed

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,21 @@
+const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+];
+
+const colorCode = function(color) {
+  return COLORS.indexOf(color);
+};
+
+module.exports = {
+  colorCode,
+  COLORS
+};
