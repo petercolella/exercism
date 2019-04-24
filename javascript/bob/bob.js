446f572c9188c529c384e7e3d899a86ac85d01a2
petercolella <peter.joseph.colella@gmail.com>
2019-04-24 00:41:50 -0400

    bob all tests passed

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -8,13 +8,17 @@ export const hey = message => {
   //
   // YOUR CODE GOES HERE
   //
-  if (message === message.toUpperCase() && message.includes('?')) {
+  if (
+    !/[a-z]/.test(message) &&
+    /[A-Z]/.test(message) &&
+    message.charAt(message.length - 1) === '?'
+  ) {
     return "Calm down, I know what I'm doing!";
-  } else if (!/[a-z]/.test(message)) {
+  } else if (!/[a-z]/.test(message) && /[A-Z]/.test(message)) {
     return 'Whoa, chill out!';
-  } else if (message.includes('?')) {
+  } else if (message.charAt(message.trim().length - 1) === '?') {
     return 'Sure.';
-  } else if (message === null || message === undefined) {
+  } else if (!message || !/\S/.test(message)) {
     return 'Fine. Be that way!';
   } else {
     return 'Whatever.';
