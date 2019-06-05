f918e3e0e3a9f64b835d74d8052b2925fb04179e
petercolella <peter.joseph.colella@gmail.com>
2019-06-04 22:48:46 -0400

    pascals-triangle recursive final

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -22,15 +22,18 @@ export class Triangle {
   }
 
   populateRowsRecursive(n, rows) {
-    if (!rows) rows = [[1]];
-    if (n <= 1) return n ? rows : [];
+    if (!rows) rows = [[1]].slice(0, n);
+    if (n <= 1) return rows;
 
-    let currentRow = [];
-    let previousRow = rows[rows.length - 1];
+    const currentRow = [];
+    const previousRow = rows[rows.length - 1];
+
     for (let i = 0; i < previousRow.length + 1; i++) {
       currentRow.push((previousRow[i - 1] || 0) + (previousRow[i] || 0));
     }
+
     rows.push(currentRow);
+
     return this.populateRowsRecursive(n - 1, rows);
   }
 }
